a712621c5eb52cf6e7f9a44843697fbb2cdf97c1
AshaniTH <164789383+AshaniTH@users.noreply.github.com>
2025-06-13 17:07:00 +0530

    add todo to DOM

--- script.js
+++ script.js
@@ -21,4 +21,36 @@
                 });
             });
     }
+      // Add todo to DOM
+    function addTodoToDOM(todo) {
+        const li = document.createElement('li');
+        li.className = 'todo-item';
+        li.dataset.id = todo.id;
+        
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = todo.completed;
+        checkbox.addEventListener('change', function() {
+            updateTodoOnServer(todo.id, this.checked);
+        });
+        
+        const span = document.createElement('span');
+        span.className = 'todo-text';
+        span.textContent = todo.text;
+        if (todo.completed) {
+            span.classList.add('completed');
+        }
+        
+        const deleteBtn = document.createElement('button');
+        deleteBtn.className = 'delete-btn';
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.addEventListener('click', function() {
+            deleteTodoOnServer(todo.id);
+        });
+        
+        li.appendChild(checkbox);
+        li.appendChild(span);
+        li.appendChild(deleteBtn);
+        todoList.appendChild(li);
+    }
     
