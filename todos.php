0b5b7a90cdb7e037c06cc97f3d3d1c8e5d86d7db
AshaniTH <164789383+AshaniTH@users.noreply.github.com>
2025-06-15 19:46:37 +0530

    create file

--- todos.php
+++ todos.php
@@ -2,3 +2,7 @@
 header('Content-Type: application/json');
 // Simple database simulation using a JSON file
 $file = 'todos.json';
+// Create file if it doesn't exist
+if (!file_exists($file)) {
+    file_put_contents($file, '[]');
+}
