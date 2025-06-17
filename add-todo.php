1aad235f3d39214775ec4947e15811c163b0c0c4
AshaniTH <164789383+AshaniTH@users.noreply.github.com>
2025-06-17 14:46:40 +0530

    add input data

--- add-todo.php
+++ add-todo.php
@@ -3,3 +3,7 @@ header('Content-Type: application/json');
 
 $file = 'todos.json';
 $todos = json_decode(file_get_contents($file), true);
+// Get input data
+$text = $_POST['text'] ?? '';
+
+if (!empty($text)) {
