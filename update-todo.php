d53b270ecb1f7be1a9e0198c9552403d996fecaa
AshaniTH <164789383+AshaniTH@users.noreply.github.com>
2025-06-18 20:02:42 +0530

    add input data section

--- update-todo.php
+++ update-todo.php
@@ -3,3 +3,24 @@ header('Content-Type: application/json');
 
 $file = 'todos.json';
 $todos = json_decode(file_get_contents($file), true);
+// Get input data
+$id = $_POST['id'] ?? '';
+$completed = isset($_POST['completed']) ? (bool)$_POST['completed'] : false;
+
+$success = false;
+
+foreach ($todos as &$todo) {
+    if ($todo['id'] === $id) {
+        $todo['completed'] = $completed;
+        $success = true;
+        break;
+    }
+}
+
+if ($success) {
+    file_put_contents($file, json_encode($todos));
+    echo json_encode(['success' => true]);
+} else {
+    echo json_encode(['error' => 'Todo not found']);
+}
+?>
